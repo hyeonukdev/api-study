97ecc6a8203b8cfbb54d83a90f4ca320fd3383cd
hyeonukdev <lego7407@gmail.com>
2021-03-04 22:59:32 +0900

    front-page-api

--- api/app.py
+++ api/app.py
@@ -17,103 +17,9 @@ class CustomJSONEncoder(JSONEncoder):
 
         return JSONEncoder.default(self, obj)
 
-def create_app(test_config = None):
-    app = Flask(__name__)
-
-    CORS(app)
-
-    app.json_encoder = CustomJSONEncoder
-
-    if test_config is None:
-        app.config.from_pyfile("config.py")
-    else:
-        app.config.update(test_config)
-
-    database     = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow = 0)
-    app.database = database
-
-    @app.route("/ping", methods=['GET'])
-    def ping():
-        return "pong"
-
-    @app.route("/sign-up", methods=['POST'])
-    def sign_up():
-        new_user    = request.json
-        # bcrypt
-        new_user["password"] = bcrypt.hashpw(password=new_user["password"].encode("utf-8"), #2
-                                             salt=bcrypt.gensalt())
-        new_user_id = insert_user(new_user)
-        new_user    = get_user(new_user_id)
-
-        return jsonify(new_user)
-
-    @app.route("/login", methods=["POST"])
-    def login():
-        credential = request.json
-        email = credential["email"] 
-        password = credential["password"] 
-        user_credential = get_user_id_and_password(email) 
-        
-
-        if user_credential and bcrypt.checkpw(password=password.encode("utf-8"),
-                                              hashed_password=user_credential["hashed_password"].encode("utf-8")): 
-            user_id = user_credential["id"]
-            payload = { 
-                "user_id": user_id,
-                "exp": datetime.utcnow() + timedelta(seconds = 60 * 60 * 24)
-            }
-
-            token = jwt.encode(payload=payload, 
-                               key=app.config["JWT_SECRET_KEY"],
-                               algorithm="HS256")
-
-            return jsonify({ 
-                "access_token": token.decode("utf-8") 
-            }) 
-        else:
-            return "", 401 
-
-    @app.route('/tweet', methods=['POST'])
-    @login_required
-    def tweet():
-        user_tweet = request.json
-        tweet      = user_tweet['tweet']
-
-        if len(tweet) > 300:
-            return '300자를 초과했습니다', 400
-
-        insert_tweet(user_tweet)
-
-        return '', 200
-
-    @app.route('/follow', methods=['POST'])
-    @login_required
-    def follow():
-        payload = request.json
-        insert_follow(payload) 
-
-        return '', 200
-
-    @app.route('/unfollow', methods=['POST'])
-    @login_required
-    def unfollow():
-        payload = request.json
-        insert_unfollow(payload)
-
-        return '', 200
-
-    @app.route('/timeline/<int:user_id>', methods=['GET'])
-    @login_required
-    def timeline():
-        user_id = g.user_id
-        return jsonify({
-            'user_id'  : user_id,
-            'timeline' : get_timeline(user_id)
-        })
 
 
-    return app
-
+    
 #########################################################
 #       Decorators
 #########################################################
@@ -138,3 +44,111 @@ def login_required(f):
             return Response(status=401) 
         return f(*args, **kwargs)
     return decorated_function
+
+    
+def create_app(test_config = None):
+    app = Flask(__name__)
+
+    CORS(app)
+
+    app.json_encoder = CustomJSONEncoder
+
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.update(test_config)
+
+    database     = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow = 0)
+    app.database = database
+
+    @app.route("/ping", methods=['GET'])
+    def ping():
+        return "pong"
+
+    @app.route("/sign-up", methods=['POST'])
+    def sign_up():
+        new_user    = request.json
+        new_user['password'] = bcrypt.hashpw(
+            new_user['password'].encode('UTF-8'),
+            bcrypt.gensalt()
+        )
+
+        new_user_id = insert_user(new_user)
+        new_user    = get_user(new_user_id)
+
+        return jsonify(new_user)
+        
+    @app.route('/login', methods=['POST'])
+    def login():
+        credential      = request.json
+        email           = credential['email']
+        password        = credential['password']
+        user_credential = get_user_id_and_password(email)
+
+        if user_credential and bcrypt.checkpw(password.encode('UTF-8'), user_credential['hashed_password'].encode('UTF-8')): 
+            user_id = user_credential['id'] 
+            payload = {     
+                'user_id' : user_id,
+                'exp'     : datetime.utcnow() + timedelta(seconds = 60 * 60 * 24)
+            }
+            token = jwt.encode(payload, app.config['JWT_SECRET_KEY'], 'HS256') 
+
+            return jsonify({
+                'user_id'      : user_id,
+                'access_token' : token.decode('UTF-8')
+            })
+        else:
+            return '', 401
+
+    @app.route('/tweet', methods=['POST'])
+    @login_required
+    def tweet():
+        user_tweet       = request.json
+        user_tweet['id'] = g.user_id
+        tweet            = user_tweet['tweet']
+
+        if len(tweet) > 300:
+            return '300자를 초과했습니다', 400
+
+        insert_tweet(user_tweet)
+
+        return '', 200
+
+    @app.route('/follow', methods=['POST'])
+    @login_required
+    def follow():
+        payload       = request.json
+        payload['id'] = g.user_id
+
+        insert_follow(payload) 
+
+        return '', 200
+
+    @app.route('/unfollow', methods=['POST'])
+    @login_required
+    def unfollow():
+        payload       = request.json
+        payload['id'] = g.user_id
+
+        insert_unfollow(payload)
+
+        return '', 200
+
+    @app.route('/timeline/<int:user_id>', methods=['GET'])
+    def timeline(user_id):
+        return jsonify({
+            'user_id'  : user_id,
+            'timeline' : get_timeline(user_id)
+        })
+
+    @app.route('/timeline', methods=['GET'])
+    @login_required
+    def user_timeline():
+        user_id = g.user_id
+
+        return jsonify({
+            'user_id'  : user_id,
+            'timeline' : get_timeline(user_id)
+        })
+
+    return app
